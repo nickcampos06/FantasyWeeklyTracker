612f943072cc1263d7e7212d2d160e32abba656f
pecacho214 <nickcampos06@gmail.com>
2025-10-28 14:20:05 -0400

    Changed parameter for detecting new week, using "current_week" attribute of league class. Matchup should only update weekly now.

--- Testing/TestAPI.py
+++ Testing/TestAPI.py
@@ -9,7 +9,9 @@ league_id = int(input("league ID: "))
 my_team = input("Team Name: ")
 # Generate League
 league = League(league_id=league_id, year=year)
-# Find the full list of matchups in the league
+# Keep Track of the current week we are playing
+current_week = league.current_week
+# Find the full list of box scores in the league
 current_matchup = league.box_scores()
 # Find out which matchup belongs to the user
 for box in current_matchup:
@@ -32,8 +34,9 @@ if team == "Away":
 while True:
     time.sleep(60)
     # If a change is detected, we update the full list of matchups and find which one is the user's once again
-    if current_matchup != league.box_scores():
+    if current_week != league.current_week:
         current_matchup = league.box_scores()
+        current_week = league.current_week
         for box in current_matchup:
             if box.home_team.team_name == my_team:
                 team = "Home"
@@ -41,13 +44,13 @@ while True:
             elif box.away_team.team_name == my_team:
                 team = "Away"
                 score = box
-        # We print the current score
-        if team == "Home":
-            print(score.home_score)
-            print(datetime.now())
-        if team == "Away":
-            print(score.away_score)
-            print(datetime.now())
+    # We print the current score
+    if team == "Home":
+        print(score.home_score)
+        print(datetime.now())
+    if team == "Away":
+        print(score.away_score)
+        print(datetime.now())
 
 # For debugging purposes, I also print the date, so we can see when the week resets precisely
 # and ensure the code accounts for it.
